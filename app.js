5c58786f9fd131b31f3bbffbc0145d6bacde39ea
Ben Tran <letscontactben@gmail.com>
2024-11-18 21:04:13 +0700

    Update endpoint app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/v1", indexRouter);
+app.use("/api", indexRouter);
 
 connection();
 
